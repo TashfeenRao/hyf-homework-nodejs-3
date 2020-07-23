53b227297208634d86413298c10bb6038623877f
tashfeenrao <raotashfeen25@gmail.com>
2020-07-24 03:55:06 +0500

    hello world text added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const server = http.createServer((request,response) => {
     response.writeHead(200,{
         "content-type": "text/plain"
     });
-    response.end("Hello World\n");
+    response.end("Hello World!");
 });
 server.listen(3000);
